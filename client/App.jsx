0d24035027a3f2a08306d64a659d84659fe98f30
Isaac Bocage <87297446+isaacb0@users.noreply.github.com>
2023-12-23 12:28:37 -0800

    rendered board component. Co-authored-by: Kevin Chou <choukevin@berkeley.edu> Co-authored-by: Chris Everett <Chris.Everett4242@gmail.com> Co-authored-by: Zoren L. <zorenzal@gmail.com>

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Homepage from './components/homepage.jsx'
 
 const App = () => {
     return (
-        <div>
-            <h1>SUPER COOL QUEST BOARD APP..</h1>
+        <div id="App">
+            <Homepage />
         </div>
     )
 }
 
 export default App;
+
+
---
+++ client/components/homepage.jsx
@@ -0,0 +1,12 @@
+ import React from 'react';
+ import Board from './board.jsx'; 
+
+ const Homepage = () => {
+   return (
+    <div id='homepage-container'>
+    <Board />
+    </div>
+   )
+ }
+
+ export default Homepage;
